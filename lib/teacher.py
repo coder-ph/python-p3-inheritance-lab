ba0a681ee36371264edde72e2465d0cfd1ae3970
Phelix <phelixmbani@gmail.com>
2024-12-02 13:51:21 +0300

    inheritance lab

--- lib/teacher.py
+++ lib/teacher.py
@@ -4,7 +4,27 @@ from user import User
 
 import random
 
+
 class Teacher(User):
+    def __init__(self, first_name, last_name, knowledge=None):
+        super().__init__(first_name, last_name)
+        
+        if knowledge and isinstance(knowledge, list) and len(knowledge)> 0:
+            self.knowledge = knowledge
+        else:
+            self.knowledge = ["default knowledge"]
+        
+    @property
+    def knowledge(self):
+        return self._knowledge
+    
+    @knowledge.setter
+    def knowledge(self, knowledge):
+        if isinstance(knowledge, list) and len(knowledge)> 0:
+            self._knowledge = knowledge
+            
+        else:
+            raise ValueError ("knowledge must be a non-empty list")
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
